c63e4a6cbbad3fa2075db9d1b9401c426a8bcba3
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-03-11 10:11:58 -0400

    saving people to local storage, issue recalling them and issue sorting them

--- www/js/person.js
+++ www/js/person.js
@@ -8,11 +8,38 @@ export const person = {
         let div = template.content.cloneNode(true);
         container.appendChild(div);
 
+        //check local storage to see if there are any items
+        let storedItems = JSON.parse(localStorage.getItem('GIFTR'));
+
+        // console.log(storedItems);
+        if(storedItems && storedItems.length > 0){ //if we have items in the stored item array
+            //first initilize the list to hold, because when they are added intially
+            //the set checks for unique ness we can just reload the people saved
+            storedItems.forEach(x => {
+                let savedPerson = JSON.parse(localStorage.getItem(`GIFTR-${x}`))
+                person.list.push(savedPerson);
+                console.log(x);
+
+                //get a reference to the ul and append the people
+                let ul = document.querySelector('.person-container ul');
+                ul.innerHTML = ""; //clear out the contents of the list
+        
+                let frag = document.createDocumentFragment();
+                person.list.forEach(friend => {
+                    let li = person.createListItem(friend);
+                    //append the li to the document frag
+                    frag.appendChild(li);
+                });
+                ul.appendChild(frag);   
+            })
+            console.log(person.list);
+            
+        }
+
         //listen for the Person Added message
         console.log('PERSON: is listening for a person added');
         pubsub.subscribe('personAdded', person.personAdded);
     },
-
 
     personAdded: newPerson =>{
         //using set to prevent duplicates
@@ -20,16 +47,7 @@ export const person = {
         let list = new Set(person.list);
         list.add(newPerson);
 
-        person.list = Array.from(list).sort((a,b) => {
-            let a_date = new Date(a.birthdate);
-            let b_date = new Date(b.birthdate);
-
-            if(a_date.getMonth() == b_date.getMonth()){ //they are the same month return day
-                return a_date.getDay() - b_date.getDay();
-            } else {
-                return a_date.getMonth() - b_date.getMonth(); //if not the same month sort by month
-            }
-        });
+        person.list = Array.from(list).sort(person.sortPerson);
 
         //update any of the modules listening about the new person update
         console.log(`People: just updated the list`);
@@ -42,29 +60,47 @@ export const person = {
         let frag = document.createDocumentFragment();
 
         person.list.forEach(friend => {
-
-            //first create the elements
-            let li = document.createElement('li');
-            let name = document.createElement('p');
-            let date = document.createElement('p');
-            let btn = document.createElement('btn');
-            let btn_label = document.createElement('p');
-
-            //set the properties and attributes needed
-            li.setAttribute('data-personid', friend.id);
-            name.textContent = friend.name;
-            date.textContent = friend.birthdate;
-            btn_label.textContent = 'DELETE'; //replace later with delete icon
-
-            //append the attributes in the needed way
-            btn.appendChild(btn_label);
-            li.appendChild(date);
-            li.appendChild(name);
-            li.appendChild(btn);
-
+            let li = person.createListItem(friend);
             //append the li to the document frag
             frag.appendChild(li);
         });
         ul.appendChild(frag);        
+    },
+
+    createListItem: friend =>{
+        //first create the elements
+        let li = document.createElement('li');
+        let name = document.createElement('p');
+        let date = document.createElement('p');
+        let btn = document.createElement('btn');
+        let btn_label = document.createElement('p');
+
+        //set the properties and attributes needed
+        li.setAttribute('data-personid', friend.id);
+        name.textContent = friend.name;
+        date.textContent = friend.birthdate;
+        btn_label.textContent = 'DELETE'; //replace later with delete icon
+
+        //append the attributes in the needed way
+        btn.appendChild(btn_label);
+        li.appendChild(date);
+        li.appendChild(name);
+        li.appendChild(btn);
+
+        return li;
+    },
+
+    //callback function used for the sort
+    //first we compared birth months then compare
+    //the birth day
+    sortPerson: (a,b) =>{
+        let a_date = new Date(a.birthdate);
+        let b_date = new Date(b.birthdate);
+
+        if(a_date.getMonth() == b_date.getMonth()){ //they are the same month return day
+            return a_date.getDay() - b_date.getDay();
+        } else {
+            return a_date.getMonth() - b_date.getMonth(); //if not the same month sort by month
+        }
     }
 };
--- www/js/personForm.js
+++ www/js/personForm.js
@@ -1,6 +1,7 @@
 import {pubsub} from './pubsub.js';
 
 export const personForm = {
+    trackerList: [], //will be used to store just the ids in local storage to have a reference of what to 
     render: container =>{
         //build form to add movie
         let template = document.getElementById('personFormTemplate');
@@ -23,12 +24,21 @@ export const personForm = {
             let id = Date.now();
             //now update the modules about the new person
             let newPerson = {id, name, birthdate}; //using the object shorthand instead of id:id
+            personForm.trackPerson(newPerson);
             pubsub.publish('personAdded', newPerson);
             nameField.value = ""; //clear out the values
             birthdateField.value = ""; 
         } else {
             alert('You Are Missing Required Attributes');
         }
-        
+    },
+
+    //in the trackPerson helper function we will save the new Person to local storage
+    //and also save their id to an array (also in local storage) to have refenrence of what
+    //to search for in local storage when loading the program
+    trackPerson: person =>{
+        personForm.trackerList.push(person.id);
+        localStorage.setItem(`GIFTR`, JSON.stringify(personForm.trackerList));
+        localStorage.setItem(`GIFTR-${person.id}`, JSON.stringify(person));
     }
 }
