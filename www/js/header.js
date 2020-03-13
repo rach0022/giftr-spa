6ee689d07cc59d7ad79cef9aa8f043b53aba5bd6
Ravi <rach0022@algonquinlive.com>
2020-03-12 22:30:43 -0400

    made header button publish events to show person/gift form depending on person selection

--- www/js/header.js
+++ www/js/header.js
@@ -1,15 +1,43 @@
 import {pubsub} from './pubsub.js';
 
 export const header = {
-
+    container: null, //get a reference to the container that loads this module
     //will initally render the container in the main.js
-    render: container =>{
+
+    //creating this load template helper function as its done multiple times
+    loadTemplate: function() {
         let template = document.getElementById('headerTemplate');
         let div = template.content.cloneNode(true);
-
+        return div; 
+    }, 
+    render: container =>{
+        header.container = container; 
+        let div = header.loadTemplate();
         //now add to the container and subscribe to other module feeds
         container.appendChild(div);
-        pubsub.subscribe('personSelected', header.changeAddButton);
+        pubsub.subscribe('personSelected', header.showGiftForm);
+        pubsub.subscribe('personDeselected', header.showPersonForm);
     },
-    changeAddButton: target => {}
+
+    showGiftForm: target =>{
+        header.container.innerHTML = "";
+        let div = header.loadTemplate();
+        div.querySelector('button').addEventListener('click', ev => {pubsub.publish('showGiftForm')});
+
+        header.container.appendChild(div);
+    },
+    showPersonForm: target => {
+        header.container.innerHTML = "";
+        let div = header.loadTemplate();
+        div.querySelector('button').addEventListener('click', ev => {pubsub.publish('showPersonForm')});
+        header.container.appendChild(div);
+    },
+
+    // revealGiftForm: ev => {
+    //     console.log('change the add button to show the add gift form');
+
+    // },
+    // revealPersonForm: ev => {
+    //     console.log('change the add button to show the add personform')
+    // }
 }
